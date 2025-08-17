a2c22a1c79b61b443aae27525c551bb9c9a9d4b4
Rick Schubert <rickschubert@gmx.de>
2025-08-17 09:49:29 +0100

    prettifying

--- src/test/scalar_support.spec.ts
+++ src/test/scalar_support.spec.ts
@@ -1,31 +1,28 @@
-import {
-    graphQlQueryToJson,
-} from "../index"
+import {graphQlQueryToJson} from "../index"
 
 describe("Scalar support", () => {
     it("Scalar query", () => {
-
         const query = `
             query fooBarAlias($name: String!, $lastName: String!) {
                 fooBar(name: $name, lastName: $lastName)
-            }`;
+            }`
 
         const newJson = graphQlQueryToJson(query, {
-        variables: {
-            name: 'PETER',
-            lastName: 'SCHMIDT',
-        },
-        });
+            variables: {
+                name: "PETER",
+                lastName: "SCHMIDT",
+            },
+        })
 
         expect(newJson).toEqual({
             query: {
                 fooBar: {
                     __args: {
                         lastName: "SCHMIDT",
-                        name: "PETER"
-                    }
-                }
-            }
-        });
+                        name: "PETER",
+                    },
+                },
+            },
+        })
     })
 })
