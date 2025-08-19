21d2bb52b6ae8fe1af6d02660bab0f6de872476e
Thomas Chaplin <thomaschaplin@outlook.com>
2025-08-19 11:18:54 +0100

    move test into other describe block

--- src/test/full_functionality.spec.ts
+++ src/test/full_functionality.spec.ts
@@ -172,6 +172,29 @@ describe("Queries", () => {
             },
         })
     })
+
+    it("Scalar field with arguments", () => {
+        const query = `
+        query fooBarAlias($name: String!, $lastName: String!) {
+            fooBar(name: $name, lastName: $lastName)
+        }`
+        const result = graphQlQueryToJson(query, {
+            variables: {
+                name: "PETER",
+                lastName: "SCHMIDT",
+            },
+        })
+        expect(result).toEqual({
+            query: {
+                fooBar: {
+                    __args: {
+                        name: "PETER",
+                        lastName: "SCHMIDT",
+                    },
+                },
+            },
+        })
+    })
 })
 
 describe("Mutations", () => {
@@ -526,29 +549,6 @@ describe("Enum Types", () => {
                     },
                     personal: {
                         count: true,
-                    },
-                },
-            },
-        })
-    })
-
-    it("Scalar field with arguments", () => {
-        const query = `
-        query fooBarAlias($name: String!, $lastName: String!) {
-            fooBar(name: $name, lastName: $lastName)
-        }`
-        const result = graphQlQueryToJson(query, {
-            variables: {
-                name: "PETER",
-                lastName: "SCHMIDT",
-            },
-        })
-        expect(result).toEqual({
-            query: {
-                fooBar: {
-                    __args: {
-                        name: "PETER",
-                        lastName: "SCHMIDT",
                     },
                 },
             },
